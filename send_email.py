21915e1d80b62008614a45c12ae646b09ae07123
ALEX DOMINGUES DA SILVA <63565501+alexdominguess@users.noreply.github.com>
2020-11-25 02:08:39 -0300

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,24 +1,21 @@
 import smtplib
 
-def send_email(user_email, user_password, subject, msg):
+def send_email(user_email, subject, msg):
 
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
     server.login("alexdominguess@gmail.com", "D46in3hh")
     message = 'Subject: {}\n\n{}'.format(subject, msg)
-    server.sendmail('alexdominguess@gmail.com',"alexdominguess@gmail.com", message)
+    server.sendmail('alexdominguess@gmail.com',user_email, message)
     server.quit()
     return "Senha enviada"
 
 
 
-
-
-
 if __name__ == '__main__':
-    user_email = 'testeteste.gmail.com'
+    user_email = 'alexdominguess@gmail.com'
     user_password = '123456'
     subject = 'Senha Recuperada'
     message = ' Aqui esta sua senha - '+ user_password
-    send_email(user_email, user_password, subject, message)
+    send_email(user_email, subject, message)

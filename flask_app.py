598738ef2101f8d4c842eceabf07ad71c6157f1d
ALEX DOMINGUES DA SILVA <63565501+alexdominguess@users.noreply.github.com>
2020-12-15 23:43:20 -0300

    last commit

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from login_functions import add_acount, login, recupera_senha, alterar_senha
 from db_functions import get_data, update_data
 from send_email import send_email
 
+
 app= Flask(__name__)
-user = [""]
+app.secret_key = "Nj!fxX<LI=[$\\}3,^`j];vS4-%De!;k0n9MQze7I&_4gLcERq[!|yS]XCTRdblocQ=*)Rz"
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == "POST":
         email = request.form.get("email")
-        user[0] = email
         pwd = request.form.get("pwd")
         if len(email) == 0 or len(pwd) == 0:
             return render_template("index.html", message = "Email ou Password não preenchido")
@@ -18,20 +18,20 @@ def home():
         if login_msg != "sucess":
             return render_template("index.html", message = login_msg)
         else:
+            session["user"] = email
             return redirect("todo")
     if request.method == "GET":
-        user[0] = "" #Requires user to login every time login page is loaded
-        
         return render_template ("index.html")
 
 
 @app.route("/todo", methods = ["GET", "POST"])
 def todo_list():
-    if user[0] != "":
-        sql = "SELECT * FROM tarefas WHERE email = "+ "'" + user[0] + "'"
-        tarefas = get_data(sql)
-        return render_template("todo.html", tarefas=tarefas, user=user[0])
-    else:
+    try:
+        if session["user"]:
+            sql = "SELECT * FROM tarefas WHERE email = "+ "'" + session["user"] + "'"
+            tarefas = get_data(sql)
+            return render_template("todo.html", tarefas=tarefas, user=session["user"])
+    except:
         return redirect("/")
 
 
@@ -39,7 +39,6 @@ def todo_list():
 def cadastrar():
     if request.method == "GET":
         return render_template ("cadastrar.html")
-
     else:
         email = request.form.get("cad_email")
         pwd = request.form.get("cad_pwd")
@@ -60,19 +59,15 @@ def recuperar_senha():
             send_email(user_email, subject, msg)
             return render_template ("recuperarsenha.html", message = "Nova senha enviada")
     else:
-        if user[0] == "":
-            return redirect("/")
-        else:
-            return render_template ("recuperarsenha.html")
+        return render_template ("recuperarsenha.html")
 
 
 @app.route("/add_tarefa", methods = ["POST"])
 
 def add_tarefa():
-    #gets tarefa from the form and add to the db
+    #gets tarefa from the form and add to db
     tarefa = request.form.get("tarefa")
-    email = request.form.get("user")
-    user[0] = email
+    email = session["user"]
     sql = "INSERT INTO tarefas(email, tarefa) VALUES('{}','{}')".format(email, tarefa)
     update_data(sql)
     return redirect("todo")
@@ -82,8 +77,6 @@ def add_tarefa():
 def delete_tarefa():
     #gets the tarefa's id and delete from db
     tarefa_id = request.form.get("tarefa_id")
-    email = request.form.get("user")
-    user[0] = email
     sql = "DELETE FROM tarefas WHERE tarefa_id = {}".format(tarefa_id)
     update_data(sql)
     return redirect("todo")
@@ -92,17 +85,18 @@ def delete_tarefa():
 @app.route("/mudar_senha", methods = ["GET","POST"])
 def mudar_senha():
     if request.method == "GET":
-        if user[0] == "":
+        try:
+            if session["user"]:
+                return render_template("mudar_senha.html", user=session["user"])
+        except:
             return redirect("/")
-        else:
-            return render_template("mudar_senha.html", user=user[0])
     else:
         pwd_atual = request.form.get("password_atual")
         pwd_novo_1 = request.form.get("novo_password_1")
         pwd_novo_2 = request.form.get("novo_password_2")
 
-        result = alterar_senha(user[0], pwd_atual, pwd_novo_1, pwd_novo_2)
-        return render_template("mudar_senha.html", user=user[0], message= result)
+        result = alterar_senha(session["user"], pwd_atual, pwd_novo_1, pwd_novo_2)
+        return render_template("mudar_senha.html", user=session["user"], message= result)
 
 
 if __name__ == '__main__':
